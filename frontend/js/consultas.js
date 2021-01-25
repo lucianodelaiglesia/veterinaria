30e9316fb6fddab47f68d20a571c512d718384b6
lucianodelaiglesia <lucianodelaiglesia@hotmail.com>
2021-01-25 17:36:20 -0300

    listar mascotas y veterinarixs en select

--- frontend/js/consultas.js
+++ frontend/js/consultas.js
@@ -1,17 +1,21 @@
 const listaConsultas = document.getElementById("lista-consultas");
-const url = 'http://localhost:5000/consultas'
+const mascota = document.getElementById("mascota");
+const veterinarix = document.getElementById("veterinarix");
+const url = 'http://localhost:5000';
 
 let consultas = [];
-
+let mascotas = [];
+let veterinarixs = [];
 
 async function listarConsultas() {
     try {
-        const respuesta = await fetch(url);
+        const entidad = "consultas";
+        const respuesta = await fetch(`${url}/${entidad}`);
         const consultasDelServer = await respuesta.json();
         if (Array.isArray(consultasDelServer)) {
             consultas = consultasDelServer;
-        }
-        if (consultas.length > 0) {
+        };
+        if (respuesta.ok) {
             const htmlConsultas = consultas.map((consulta, index) =>
                 `<tr>
                 <th scope="row">${index}</th>
@@ -22,8 +26,7 @@ async function listarConsultas() {
                 <td>${consulta.fechaEdicion}</td>
                 <td>
                     <div class="btn-group" role="group" aria-label="Basic example">
-                        <button type="button" class="btn btn-primary editar"><i class="bi bi-pencil"></i></button>
-                        <button type="button" class="btn btn-danger eliminar"><i class="bi bi-trash"></i></button>
+                        <button type="button" class="btn btn-primary editar"><i class="bi bi-pencil"> Editar</i></button>
                     </div>
                 </td>
                 </tr>`).join("");
@@ -31,7 +34,53 @@ async function listarConsultas() {
         };
     } catch (error) {
         $(".alert").show();
-    }
-}
+    };
+};
 
 listarConsultas();
+
+async function listarMascotas() {
+    const entidad = "mascotas";
+    try {
+        const respuesta = await fetch(`${url}/${entidad}`);
+        const mascotasDelServidor = await respuesta.json();
+        if (Array.isArray(mascotasDelServidor)) {
+            mascotas = mascotasDelServidor;
+        };
+        if (respuesta.ok) {
+            mascotas.forEach((_mascota, index) => {
+                const optionActual = document.createElement("option");
+                optionActual.innerHTML = _mascota.nombre;
+                optionActual.value = index;
+                mascota.appendChild(optionActual);
+            });
+        };
+    } catch (error) {
+        $(".alert").show();
+    };
+};
+
+listarMascotas();
+
+async function listarVeterinarixs() {
+    const entidad = "veterinarixs";
+    try {
+        const respuesta = await fetch(`${url}/${entidad}`);
+        const veterinarixsDelServidor = await respuesta.json();
+        if (Array.isArray(veterinarixsDelServidor)) {
+            veterinarixs = veterinarixsDelServidor;
+        };
+        if (respuesta.ok) {
+            veterinarixs.forEach((_veterinarix, index) => {
+                const optionActual = document.createElement("option");
+                optionActual.innerHTML = `${_veterinarix.nombre} ${_veterinarix.apellido}`;
+                optionActual.value = index;
+                veterinarix.appendChild(optionActual);
+            });
+        };
+    } catch (error) {
+        $(".alert").show();
+    };
+};
+
+listarVeterinarixs();
